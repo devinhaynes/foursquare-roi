9560e79685bb22cd6bb624675bbad2648b89b0d1
Devin Haynes <dhaynes916@gmail.com>
2025-10-01 13:00:26 -0400

    Patched middleware error. Fixed linting errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,12 @@
-"use client";
+import { redirect } from "next/navigation";
+import { createClient } from "@/lib/supabase/server";
+export default async function Home() {
+  const supabase = await createClient();
 
-import { createClient } from "@/lib/supabase/client";
-import { useRouter } from "next/navigation";
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
 
-export default function Home() {
-  const router = useRouter();
-
-  const routeSession = async () => {
-    const supabase = createClient();
-    const {
-      data: { session },
-      error,
-    } = await supabase.auth.getSession();
-
-    if (error) {
-      console.error("Error getting session:", error.message);
-      // Handle error appropriately
-    } else if (session) {
-      router.replace("/calc");
-      // User is logged in, you can access session.user for user details
-    } else {
-      router.replace("/login");
-      console.log("User is not logged in.");
-      // User is not logged in
-    }
-  };
-
-  routeSession();
+  if (session) redirect("/calc");
+  redirect("/login");
 }
