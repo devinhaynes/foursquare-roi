c9cf36a61029d1d11b83655d18c7c022d260a2f7
Devin Haynes <dhaynes916@gmail.com>
2026-03-10 15:32:56 -0500

    Layout shift and removed supabase

--- app/components/Toolbar.tsx
+++ app/components/Toolbar.tsx
@@ -15,10 +15,10 @@ export const Toolbar = () => {
     "mx-1 my-2 text-accent rounded-full p-2 hover:bg-zinc-200 hover:dark:bg-zinc-900 transition-all | md:rounded-none md:mx-0 md:my-0 md:px-2 md:py-3";
   return (
     <div className="md:h-full">
-      <div className="hidden md:grid bg-white outline-1 outline-accent dark:bg-zinc-950 rounded-full mt-4 ml-4 w-[52px] h-[52px] place-content-center">
+      <div className="hidden md:grid bg-white outline-1 outline-accent dark:bg-zinc-950 rounded-full mt-4 ml-4 w-13 h-13 place-content-center">
         <Image src={logo} alt="" className="p-3" />
       </div>
-      <div className="border-t-[1px] border-t-accent max-h-[calc(100vh-30px)] bg-white dark:bg-zinc-950 dark:border-t-zinc-800 shadow-[6px_0_12px_-2px_#c3c3c3] dark:shadow-[6px_0_8px_-2px_#1e1e1e] | md:shadow-[0_0_8px_#c3c3c3] md:dark:shadow-[0_0_4px_#1e1e1e] md:m-4 md:border-0 md:outline-1 md:outline-accent md:rounded-full md:w-fit md:sticky md:top-4 md:mt-4 md:h-full md:max-h-[calc(100vh-52px-40px)]">
+      <div className="border-t border-t-accent max-h-[calc(100vh-30px)] bg-white dark:bg-zinc-950 dark:border-t-zinc-800 shadow-[6px_0_12px_-2px_#c3c3c3] dark:shadow-[6px_0_8px_-2px_#1e1e1e] | md:shadow-[0_0_8px_#c3c3c3] md:dark:shadow-[0_0_4px_#1e1e1e] md:m-4 md:border-0 md:outline-1 md:outline-accent md:rounded-full md:w-fit md:sticky md:top-4 md:mt-4 md:h-full md:max-h-[calc(100vh-52px-40px)]">
         <nav className="text-4xl flex md:flex-col md:h-full justify-center">
           <div className="md:grow flex gap-x-4 items-center md:flex-col">
             <Link
@@ -44,7 +44,7 @@ export const Toolbar = () => {
               <TbHomeStar />
             </Link>
           </div>
-          <div className="flex md:ml-0 md:flex-col md:border-t-[1px] md:border-t-zinc-300 md:dark:border-t-zinc-800">
+          <div className="flex md:ml-0 md:flex-col md:border-t md:border-t-zinc-300 md:dark:border-t-zinc-800">
             <Link
               href="settings"
               className={
